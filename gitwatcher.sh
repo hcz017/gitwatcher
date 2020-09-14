d93838eddb4a8f45ba2c90ca031595233d6e7a22
hcz017 <hcz017@gmail.com>
2020-09-14 13:49:04 +0800

    update cript, add more comments

--- gitwatcher.sh
+++ gitwatcher.sh
@@ -3,15 +3,18 @@ function whether_changed(){
     local file_path=${1}
     local check_time=${2}
 
-    file_old_stat="`stat ${file_path}|grep Modify`"
+    # return commond exe value, format:
+    # variable=`commands`
+    # variable=$(commands)
+    file_old_stat=`stat ${file_path}|grep Modify`
     sleep ${check_time}
-    file_new_stat="`stat ${file_path}|grep Modify`"
-    if [[ `echo ${file_old_stat}` != `echo ${file_new_stat}` ]];
+    file_new_stat=$(stat ${file_path}|grep Modify)
+    if [[ $(echo ${file_old_stat}) != $(echo ${file_new_stat}) ]];
     then
-        if [[ `git diff ${file_path}` != `echo` ]];
+        if [[ $(git diff ${file_path}) != $echo ]];
         then
             echo "file ${file_path} changed! execute git add"
-            `git add ${file_path}`
+            $(git add ${file_path})
         else
             echo "file ${file_path} changed! but not tracked by git"
         fi
@@ -19,12 +22,14 @@ function whether_changed(){
 }
 
 function read_dir(){
-for file in `ls $1`
+for file in $(ls $1)
 do
  if [ -d $1"/"$file ]
  then
+ # // if sub dir, recursive call read_dir function
  read_dir $1"/"$file
  else
+ # echo value > return value 
  echo $1"/"$file
  fi
 done
@@ -33,8 +38,10 @@ done
 echo "Big brother is watching you"
 echo "Files under $1 will be checked every ${2} second"
 while [[ true ]]; do
-    for file in `read_dir $1`
+    # call read_dir function to list all files
+    for file in $(read_dir $1)
     do
+        # cmd end with '&', run background
         whether_changed $file $2 &
     done
     wait
